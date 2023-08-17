b6fde939818a4683952dca778cb50e3643fb06ae
Ayesha Arshad <ayesha.arshad@A006-00643.local>
2023-08-17 01:16:40 -0700

    Updated_Celery

--- trendico/tests/test_views.py
+++ trendico/tests/test_views.py
@@ -4,46 +4,60 @@ from django.urls import reverse
 from trendico.forms import SignUpForm
 from trendico.models import ProductCategory, Product, Wishlist, Cart, CartItem
 
+
 class HomeViewTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.category = ProductCategory.objects.create(name='Test Category')
-        self.product1 = Product.objects.create(name='Product 1', price=10.00, initial_quantity=10, category=self.category)
-        self.product2 = Product.objects.create(name='Product 2', price=15.00, initial_quantity=5, category=self.category)
+        self.product1 = Product.objects.create(
+            name='Product 1', price=10.00, initial_quantity=10, category=self.category)
+        self.product2 = Product.objects.create(
+            name='Product 2', price=15.00, initial_quantity=5, category=self.category)
 
     def test_home_view(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
-        self.assertEqual(len(response.context['categories']), ProductCategory.objects.count())
-        self.assertEqual(len(response.context['Products']), Product.objects.count())
+        self.assertEqual(
+            len(response.context['categories']), ProductCategory.objects.count())
+        self.assertEqual(
+            len(response.context['Products']), Product.objects.count())
         self.assertIsNone(response.context['selected_category'])
+
 
 class RemoveFromCartViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
         self.cart = Cart.objects.create(user=self.user)
-        self.cart_item = CartItem.objects.create(cart=self.cart, product_id=1, quantity=2)
+        self.cart_item = CartItem.objects.create(
+            cart=self.cart, product_id=1, quantity=2)
 
     def test_remove_cart_item(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.post('/remove_from_cart/{}/'.format(self.cart_item.id))
+        response = self.client.post(
+            '/remove_from_cart/{}/'.format(self.cart_item.id))
         self.assertEqual(response.status_code, 200)
+
 
 class RemoveFromWishlistViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.category = ProductCategory.objects.create(name='Test Category')  # Create a category instance
-        self.product = Product.objects.create(name='Test Product', price=10.00, initial_quantity=10, category=self.category)
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        self.category = ProductCategory.objects.create(
+            name='Test Category')  # Create a category instance
+        self.product = Product.objects.create(
+            name='Test Product', price=10.00, initial_quantity=10, category=self.category)
         self.wishlist = Wishlist.objects.create(user=self.user)
         self.wishlist.items.add(self.product)
 
     def test_remove_wishlist_item(self):
         self.client.login(username='testuser', password='testpassword')
-        response = self.client.post(reverse('remove_from_wishlist', args=[self.product.id]))
+        response = self.client.post(
+            reverse('remove_from_wishlist', args=[self.product.id]))
         self.assertEqual(response.status_code, 200)
 
 
@@ -57,6 +71,7 @@ class TestLaptopCategoryView(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+
 class TestTabCategoryView(TestCase):
     def setUp(self):
         self.client = Client()
@@ -66,6 +81,7 @@ class TestTabCategoryView(TestCase):
         url = reverse('product-category', args=[self.category])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
 
 class TestHeadphonesCategoryView(TestCase):
     def setUp(self):
@@ -77,11 +93,14 @@ class TestHeadphonesCategoryView(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
+
 class ProductViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.category = ProductCategory.objects.create(name='Test Category')  # Create a category instance
-        self.product = Product.objects.create(name='Test Product', price=10.00, initial_quantity=10, category=self.category)  # Provide valid category value
+        self.category = ProductCategory.objects.create(
+            name='Test Category')  # Create a category instance
+        self.product = Product.objects.create(
+            name='Test Product', price=10.00, initial_quantity=10, category=self.category)  # Provide valid category value
         self.url = reverse('product', args=[self.product.name])
 
     def test_product_view_exists(self):
@@ -92,10 +111,12 @@ class ProductViewTest(TestCase):
         response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'product.html')
 
+
 class LoginViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
         self.url = reverse('login')
 
     def test_login_view_exists(self):
@@ -107,19 +128,23 @@ class LoginViewTest(TestCase):
         self.assertTemplateUsed(response, 'login.html')
 
     def test_login_view_successful_login(self):
-        response = self.client.post(self.url, {'username': 'testuser', 'password': 'testpassword'})
+        response = self.client.post(
+            self.url, {'username': 'testuser', 'password': 'testpassword'})
         self.assertRedirects(response, reverse('home'))
+
 
 class LogoutViewTest(TestCase):
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
         self.client.login(username='testuser', password='testpassword')
         self.url = reverse('logout')
 
     def test_logout_view_successful_logout(self):
         response = self.client.get(self.url)
         self.assertRedirects(response, reverse('home'))
+
 
 class RegisterViewTest(TestCase):
     def setUp(self):
