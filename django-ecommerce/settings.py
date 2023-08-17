a325fa550afe9c75f9e604cf2de1312b66a0410a
Ayesha Arshad <ayesha.arshad@A006-00643.local>
2023-08-17 01:12:09 -0700

    Final-Commit

--- django-ecommerce/settings.py
+++ django-ecommerce/settings.py
@@ -145,5 +145,4 @@ EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv(
-    'DEFAULT_FROM_EMAIL', 'ayesha.arshad@arbisoft.com')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'ayesha.arshad@arbisoft.com')
--- trendico/tasks.py
+++ trendico/tasks.py
@@ -4,9 +4,11 @@ from django.core.mail import EmailMessage
 from celery import shared_task
 import smtplib
 
+
 @shared_task
 def send_order_notification_email(order_id):
-    admin_emails = list(User.objects.filter(is_staff=True, is_superuser=True).values_list('email', flat=True))
+    admin_emails = list(User.objects.filter(
+        is_staff=True, is_superuser=True).values_list('email', flat=True))
     subject = 'New Order Placed'
     message = f'An order with ID {order_id} has been placed.'
 
